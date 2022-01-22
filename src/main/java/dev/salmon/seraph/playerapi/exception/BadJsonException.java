9de8cedc9c9c9be9bcbbff3f73fae739b182335a
Sam <90007553+Scherso@users.noreply.github.com>
2022-01-21 19:46:24 -0500

    feat: new chat info, and /seraph info subcommand

--- src/main/java/dev/salmon/seraph/playerapi/exception/BadJsonException.java
+++ src/main/java/dev/salmon/seraph/playerapi/exception/BadJsonException.java
@@ -8,7 +8,7 @@ import net.minecraft.util.ChatComponentText;
 public class BadJsonException extends Exception {
 
     public BadJsonException() {
-        Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(References.SERAPHPREFIX + ChatColor.GRAY + "Hypixel API returned an bad json, Hypixel API may be down. If it is not, please contact a developer about this."));
+        Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(References.SERAPHPREFIX + ChatColor.RED + "Hypixel API returned an bad json, Hypixel API may be down. If it is not, please contact a developer about this."));
     }
 
 }
