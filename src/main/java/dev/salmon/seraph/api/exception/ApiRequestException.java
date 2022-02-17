379b96f2238237d0aa78d1e7b3cf8aa75038dd4e
Sam <90007553+Scherso@users.noreply.github.com>
2022-02-17 18:38:38 -0500

    fix(exceptions): commited package chaneges

---
+++ src/main/java/dev/salmon/seraph/api/exception/ApiRequestException.java
@@ -0,0 +1,14 @@
+package dev.salmon.seraph.api.exception;
+
+import dev.salmon.seraph.Seraph;
+import gg.essential.universal.ChatColor;
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.ChatComponentText;
+
+public class ApiRequestException extends Exception {
+
+    public ApiRequestException() {
+        Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(Seraph.Prefix.SeraphPrefix + ChatColor.RED + "There was an API request exception. If this persists, contact a developer."));
+    }
+
+}
