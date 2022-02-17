55cd2c91e8359b9316ee5ac115b2c255a2789049
Sam <90007553+Scherso@users.noreply.github.com>
2022-02-17 18:38:38 -0500

    fix(exceptions): commited package chaneges

---
+++ src/main/java/dev/salmon/seraph/api/exception/BadJsonException.java
@@ -0,0 +1,14 @@
+package dev.salmon.seraph.api.exception;
+
+import dev.salmon.seraph.Seraph;
+import gg.essential.universal.ChatColor;
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.ChatComponentText;
+
+public class BadJsonException extends Exception {
+
+    public BadJsonException() {
+        Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(Seraph.Prefix.SeraphPrefix + ChatColor.RED + "Hypixel API returned an bad json, Hypixel API may be down. If it is not, please contact a developer about this."));
+    }
+
+}
---
+++ src/main/java/dev/salmon/seraph/api/exception/InvalidKeyException.java
@@ -0,0 +1,14 @@
+package dev.salmon.seraph.api.exception;
+
+import dev.salmon.seraph.Seraph;
+import gg.essential.universal.ChatColor;
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.ChatComponentText;
+
+public class InvalidKeyException extends Exception {
+
+    public InvalidKeyException() {
+        Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(Seraph.Prefix.SeraphPrefix + ChatColor.RED + "It seems your API key is empty or invalid. Please run " + ChatColor.BOLD + "/api new"));
+    }
+
+}
