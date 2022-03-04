31eacfc52e654ac2fda629474ef2bde0560ea74b
Sam <90007553+Scherso@users.noreply.github.com>
2022-03-03 21:42:23 -0500

    fix(listener): removed redundant import

--- src/main/java/dev/salmon/seraph/listener/ApiKeyListener.java
+++ src/main/java/dev/salmon/seraph/listener/ApiKeyListener.java
@@ -7,7 +7,6 @@ import dev.salmon.seraph.util.chat.ChatColor;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.ChatComponentText;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
-import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.util.concurrent.TimeUnit;
