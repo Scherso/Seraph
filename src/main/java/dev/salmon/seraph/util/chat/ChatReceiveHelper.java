aef1b3a262c5f2e0aac58cb7eeaa83c11cc43ffd
Youded-byte <82912625+Youded-byte@users.noreply.github.com>
2022-03-16 14:55:06 +0100

    Fix Spelling

--- src/main/java/dev/salmon/seraph/util/chat/ChatReceiveHelper.java
+++ src/main/java/dev/salmon/seraph/util/chat/ChatReceiveHelper.java
@@ -3,6 +3,6 @@ package dev.salmon.seraph.util.chat;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import org.jetbrains.annotations.NotNull;
 
-public interface ChatReceieveHelper extends ChatHelper {
+public interface ChatReceiveHelper extends ChatHelper {
     void onMessageReceived(@NotNull ClientChatReceivedEvent event);
 }
