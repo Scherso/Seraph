d1f91e6d12ae0e6b2a511b7aa1a2e9d9f9c89852
Sam <90007553+Scherso@users.noreply.github.com>
2022-03-25 20:44:31 -0400

    Delete redundant classes

--- src/main/java/dev/salmon/seraph/util/chat/ChatReceiveHelper.java
+++
@@ -1,8 +0,0 @@
-package dev.salmon.seraph.util.chat;
-
-import net.minecraftforge.client.event.ClientChatReceivedEvent;
-import org.jetbrains.annotations.NotNull;
-
-public interface ChatReceiveHelper extends ChatHelper {
-    void onMessageReceived(@NotNull ClientChatReceivedEvent event);
-}
