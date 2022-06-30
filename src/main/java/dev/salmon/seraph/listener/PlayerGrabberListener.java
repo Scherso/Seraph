7969a22a7c98adea967a5b1acc043daff26856f0
Scherso <90007553+Scherso@users.noreply.github.com>
2022-06-30 15:24:11 -0400

    TEMP: Test for hypixel api stat fetching

--- src/main/java/dev/salmon/seraph/listener/PlayerGrabberListener.java
+++ src/main/java/dev/salmon/seraph/listener/PlayerGrabberListener.java
@@ -33,15 +33,15 @@ public class PlayerGrabberListener {
                             if (!PlayerUtils.isValidPlayer(playerName)) return; // This is not a real player.
                             String uuid = PlayerUtils.getUuid(playerName).toString();
                             // If it's a real player, just print their name for testing.
-                            Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText("[Test] Name: " + playerName + "\nUUID: " + uuid));
 
                             // Get player stats
                             String wins = HypixelAPI.getDuelsWins(uuid);
                             String losses = HypixelAPI.getDuelsLosses(uuid);
-                            String kills = HypixelAPI.getDuelsKills(uuid);
-                            String deaths = HypixelAPI.getDuelsDeaths(uuid);
+                            //String kills = HypixelAPI.getDuelsKills(uuid);
+                            //String deaths = HypixelAPI.getDuelsDeaths(uuid);
                             double wlr = Double.parseDouble(wins) / Double.parseDouble(losses);
-                            double kdr = Double.parseDouble(kills) / Double.parseDouble(deaths);
+                            // double kdr = Double.parseDouble(kills) / Double.parseDouble(deaths);
+                            Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText("Name: " + playerName + "\nUUID: " + uuid + "\nWins: " + wins + " Loss: " + losses + " W/L: " + wlr));
                         });
                     }
                 }
