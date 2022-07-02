3100da5278cdf00e1ce4988f0d3cc325a8e00ad1
Deftu <deftudev@gmail.com>
2022-07-02 17:08:20 +0200

    Update UCGT.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,25 +3,21 @@ import xyz.unifycraft.gradle.utils.GameSide
 plugins {
     kotlin("jvm") version("1.6.21")
     java
-    id("xyz.unifycraft.gradle.tools") version("1.6.0")
-    id("xyz.unifycraft.gradle.tools.loom") version("1.6.0")
-    id("xyz.unifycraft.gradle.tools.shadow") version("1.6.0")
-    id("xyz.unifycraft.gradle.tools.blossom") version("1.6.0")
-}
-
-loom {
-    forge {
-        mixin.defaultRefmapName.set("mixins.${modData.id}.refmap.json")
-    }
+    id("xyz.unifycraft.gradle.tools") version("1.6.1")
+    id("xyz.unifycraft.gradle.tools.loom") version("1.6.1")
+    id("xyz.unifycraft.gradle.tools.shadow") version("1.6.1")
+    id("xyz.unifycraft.gradle.tools.blossom") version("1.6.1")
 }
 
 unifycraft {
     useEssential()
+    useDevAuth()
 }
 
 loomHelper {
     disableRunConfigs(GameSide.SERVER)
     useForgeMixin(modData.id)
+    useMixinRefMap(modData.id)
     useTweaker("gg.essential.loader.stage0.EssentialSetupTweaker")
 }
 
